679c36c9c4bf7de73249f72a6b0ff245f6d93a4e
jazzdaniel <rustu.pai@gmail.com>
2025-10-28 17:03:26 -0300

    dang, it's working but I can't pass arguments

--- home.gd
+++ home.gd
@@ -11,7 +11,8 @@ extends Panel
 @export var _8: Button
 @export var date_field:Label #should change to actual label later
 
-var reservation_page=load('res://reserva.tscn')
+
+signal room_selected(room_id:int,date:String)
 
 func _ready() -> void:
 	for i in range(1, 9):
@@ -19,10 +20,7 @@ func _ready() -> void:
 		btn.connect("pressed", Callable(self, "_on_room_pressed").bind(i))
 
 func _on_room_pressed(room_number: int):
-	var date = date_field.text
-	var new_scene = reservation_page.instantiate()
-	new_scene.set_room_data(room_number, date)
-	get_tree().root.add_child(new_scene)
-	get_tree().current_scene.queue_free()
-	get_tree().current_scene = new_scene
+	#room_selected.emit(room_number,"28out2025")
+	room_selected.emit()
 	
+	print('room pressed')
--- main.gd
+++ main.gd
@@ -1,27 +1,41 @@
 extends Control
 
-#@export var _1: Button
-#@export var _2: Button
-#@export var _3: Button
-#@export var _4: Button
-#@export var _5: Button
-#@export var _6: Button
-#@export var _7: Button
-#@export var _8: Button
-#@export var date_field:Label #should change to actual label later
-#
-#var reservation_page=load('res://reserva.tscn')
-#
-#func _ready() -> void:
-	#for i in range(1, 9):
-		#var btn = get("_" + str(i))
-		#btn.connect("pressed", Callable(self, "_on_room_pressed").bind(i))
-#
-#func _on_room_pressed(room_number: int):
-	#var date = date_field.text
-	#var new_scene = reservation_page.instantiate()
-	#new_scene.set_room_data(room_number, date)
-	#get_tree().root.add_child(new_scene)
-	#get_tree().current_scene.queue_free()
-	#get_tree().current_scene = new_scene
-	#
+
+@export var page_container:Control
+var home =preload("res://home.tscn")
+var reservation = preload("res://reservation.tscn")
+
+
+var child
+
+func _ready() -> void:
+	go_to_home()
+
+
+func go_to_home():
+	#remove current page
+	clear_page_container()
+	child=home.instantiate()
+	
+	print(child.room_selected)
+	child.room_selected.connect(go_to_reservation)
+	
+	page_container.add_child(child)
+	
+func go_to_reservation(room:int=5,date:String="28/out/2025"):
+	print(room,date)
+	clear_page_container()
+	child=reservation.instantiate()
+	page_container.add_child(child)
+	
+	
+
+#region Aux funcitons#
+func clear_page_container():
+	child=page_container.get_children()
+	if child != []:
+		child[0].queue_free()
+#endregion
+
+func tester():
+	print('tester function')
