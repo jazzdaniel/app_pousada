a097e1a8bcae4dd1827b16ddf82f2c0139cfc4d7
jazzdaniel <rustu.pai@gmail.com>
2025-11-30 20:30:00 -0300

    day is highlighted

--- date_button.gd
+++ date_button.gd
@@ -6,3 +6,15 @@ var calendar_ref
 func _ready() ->void:
 	
 	text=str(date.day)
+	highlight_today()
+
+func highlight_today():
+	print('called highlight today')
+	if calendar_ref.year_today==date.year:
+		print('first test')
+		if calendar_ref.month_today==date.month:
+			print('passed second test')
+			if calendar_ref.day_today==date.day:
+					print ('passed last test')
+					self.theme_type_variation=("button_callendar_today")
+					
